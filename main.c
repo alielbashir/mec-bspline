2d635cc28813070883e2f4fb7ffcf68b866fadda
Ali Elbashir <alielbashir00@gmail.com>
2020-11-05 04:09:16 +0300

    Add piecewise bezier functionality

--- main.c
+++ main.c
@@ -1,21 +1,24 @@
 // TODO: Make noktalar dynamic according to the number of points in koordinatlar.txt
-// TODO: Implement Welzl's algorithm for finding a minimum enclosing circle (MEC)
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_primitives.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-#include <time.h>
 #define genislik 800
 #define yukseklik 800
+// new width and height
+#define wh 40
+// scale factor for points conversion
+#define sf genislik/wh
+
 
 typedef struct{
     double x;
     double y;
 }Nokta;
 
-Nokta noktalar[10];
+Nokta noktalar[100];
 typedef struct{
     Nokta p; // merkezi
     double r; // yaricapi
@@ -28,8 +31,11 @@ double mesafe(Nokta, Nokta);
 bool icinde_mi(Nokta, Cember);
 Nokta koordinati_donustur(Nokta a){
     // girilen Nokta degerleri, allegronun koordinat sistemine donusturen bir fonksiyon
-    a.x += genislik / 2;
-    a.y = yukseklik / 2 - a.y;
+    // wh is the new max height and width
+    a.x *= sf;
+    a.y *= sf;
+    a.x = (genislik / 2) + a.x ;
+    a.y = (yukseklik / 2) - a.y;
     return a;
 }
 int dosya_oku(){
@@ -65,18 +71,24 @@ void koordinat_eksenlerini_ciz(){
     al_draw_line(genislik / 2, yukseklik, genislik / 2, 0, al_map_rgb (0 , 0 , 0 ), 1);
     al_draw_line(0, yukseklik / 2, genislik, yukseklik / 2, al_map_rgb (0 , 0 , 0 ), 1);
     // kucuk cizgileri cizen dongu
-    for (i = 0; i < yukseklik; i+=20){
+    for (i = 0; i < yukseklik; i+=sf){
+        // kucuk cizgiler
         al_draw_line(genislik / 2 - 6, i, genislik / 2 + 5, i, al_map_rgb (0 , 0 , 0 ), 1 );
         al_draw_line(i, yukseklik / 2 - 6, i, yukseklik / 2 + 5, al_map_rgb (0 , 0 , 0 ), 1);
-
+        // buyuk hafif cizgiler
+        al_draw_line(0, i, genislik, i, al_map_rgba (0 , 0 , 0, 30 ), 1 );
+        al_draw_line(i,0, i, yukseklik, al_map_rgba (0 , 0 , 0, 30 ), 1);
     }
 }
-void noktalari_ciz(){
-    int i;
-    for (i = 0; i < 10; i++){
-        al_draw_filled_circle(noktalar[i].x, noktalar[i].y, 2.5, al_map_rgb ( 0 , 0 , 255 ));
+void noktalari_ciz(int m){
+    for (int i = 0; i < m; i++){
+        al_draw_filled_circle(noktalar[i].x, noktalar[i].y, 4, al_map_rgb ( 0 , i*25.5 , i*25.5 ));
     }
-
+}
+void cizgileri_ciz(int m){
+    for (int i = 0; i < m-1; i++){
+        al_draw_line(noktalar[i].x, noktalar[i].y, noktalar[i + 1].x, noktalar[i + 1].y, al_map_rgba(0, 0, 0, 62.5), 1);
+    }
 }
 // P = set of points, m = number of points in P
 // S = subset of P with points on boundary, n = number of points in S
@@ -163,7 +175,50 @@ bool icinde_mi(Nokta a, Cember mec){
 void meci_ciz(Cember mec){
     al_draw_circle(mec.p.x, mec.p.y, mec.r, al_map_rgb ( 0 , 0 , 255 ), 1);
 }
-void ekran(Cember mec){
+Nokta quadratic_bezier(Nokta a, Nokta b, Nokta c, double t){
+    Nokta pFinal;
+    pFinal.x = pow(1 - t, 2) * a.x + (1 - t) * 2 * t * b.x + t * t * c.x;
+    pFinal.y = pow(1 - t, 2) * a.y + (1 - t) * 2 * t * b.y + t * t * c.y;
+    return pFinal;
+
+}
+void piecewise_bezier(Nokta a, Nokta b, Nokta c) {
+    Nokta p1 = a;
+    Nokta p2;
+    for (double i = 0; i < 1; i+=0.1){
+        p2 = quadratic_bezier(a, b, c, i);
+        al_draw_line(p1.x, p1.y, p2.x, p2.y, al_map_rgb ( 255 , 0 , 0 ), 1);
+        p1 = p2;
+    }
+}
+Nokta midpoint(Nokta a, Nokta b){
+    Nokta p;
+    p.x = (a.x + b.x) / 2;
+    p.y = (a.y + b.y) / 2;
+    return p;
+}
+void bezieri_ciz(Nokta *P, int n) {
+    Nokta mps[n-3];
+    for (int i = 0; i < n - 3; i++){
+        mps[i] = midpoint(P[i + 1], P[i + 2]);
+    }
+    // midpoints check
+    for (int i = 0; i < n - 3; i++){
+        al_draw_filled_circle(mps[i].x, mps[i].y, 1, al_map_rgb ( 0 , 0 , 0 ));
+    }
+    for (int i = 0; i < n - 2; i++){
+        if (i > 0 && i < n - 3)
+            piecewise_bezier(mps[i - 1], P[i + 1], mps[i]);
+            // first bezier curve
+        else if (i == 0)
+            piecewise_bezier(P[i], P[i + 1], mps[i]);
+            // last bezier curve
+        else
+            piecewise_bezier(mps[i - 1], P[i + 1], P[i + 2]);
+
+    }
+}
+void ekran(Cember mec, int m){
 
     ALLEGRO_DISPLAY * disp =NULL;//
 
@@ -181,8 +236,11 @@ void ekran(Cember mec){
     //vvvvvvvvvvvvvvvvvvv-CIZIM KODU-vvvvvvvvvvvvvvvvvvv
 
     koordinat_eksenlerini_ciz();
-    noktalari_ciz();
+    noktalari_ciz(m);
+    cizgileri_ciz(m);
     meci_ciz(mec);
+    bezieri_ciz(noktalar, m);
+
 
     //^^^^^^^^^^^^^^^^^^^-CIZIM KODU-^^^^^^^^^^^^^^^^^^^
     al_flip_display();
@@ -195,22 +253,23 @@ int main()
     int m = dosya_oku();
     int i;
     Nokta bosDizi[3];
-    for(i=0;i<10;i++){
+    for(i=0;i<m;i++){
         printf ("p%d:{%.0f,%.0f} \n",i + 1, noktalar[i].x,noktalar[i].y);
     }
-    // zaman hesaplama
-    clock_t t;
+    // TODO: add timer and conduct tests for time complexity (zaman karmasikligi)
+    for (i = 0; i < m; i++){
+        noktalar[i] = koordinati_donustur(noktalar[i]);
+    }
+    for(i=0;i<m;i++){
+        printf ("p%d:{%.0f,%.0f} \n",i + 1, noktalar[i].x,noktalar[i].y);
+    }
     Cember mec = Welzl(noktalar, m, bosDizi, 0);
-
+    // FIXME: shows coordinate values after transformation. find way to detransform for display purposes
     printf("\n");
     printf("x: %f\n", mec.p.x);
     printf("y: %f\n", mec.p.y);
     printf("radius: %f\n", mec.r);
-    for (i = 0; i < 10; i++){
-        noktalar[i] = koordinati_donustur(noktalar[i]);
-    }
-    mec.p = koordinati_donustur(mec.p);
 
-    ekran(mec);
+    ekran(mec, m);
     return 0;
 }
